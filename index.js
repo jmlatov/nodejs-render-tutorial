43d03454f6d60da1d7bf6b87203d0ae99099a5de
jmlatov <jmlatov@gmail.com>
2025-05-26 11:12:08 +0200

    Primer commit

---
+++ index.js
@@ -0,0 +1,10 @@
+import express from 'express';
+
+const app = express()
+
+app.get('/', (req, res) => {
+    res.send('Hola, Mundo mundial')
+})
+
+app.listen(3000)
+console.log('Servidor en port', 3000)
